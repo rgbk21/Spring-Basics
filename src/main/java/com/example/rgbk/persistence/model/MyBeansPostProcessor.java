759b62aa258ae97008c3a117fc41314f4cfdbaa2
rgbk21 <rajgaurav.bk@gmail.com>
2020-10-22 11:06:12 -0400

    BeanPostProcessor added

---
+++ src/main/java/com/example/rgbk/persistence/model/MyBeansPostProcessor.java
@@ -0,0 +1,25 @@
+package com.example.rgbk.persistence.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBeansPostProcessor implements BeanPostProcessor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MyBeansPostProcessor.class);
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        LOG.info("Before initialising the bean: {}", beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        LOG.info("After initialising the bean: {}", beanName);
+        return bean;
+    }
+}
