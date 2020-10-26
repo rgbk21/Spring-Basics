25db9eaa5db8fd3e340e184f1caf641995526472
rgbk21 <rajgaurav.bk@gmail.com>
2020-10-26 14:22:04 -0400

    End of M3L6

--- src/test/java/com/example/rgbk/ProjectServiceIntegrationTest.java
+++ src/test/java/com/example/rgbk/ProjectServiceIntegrationTest.java
@@ -5,28 +5,54 @@ import com.example.rgbk.service.IProjectService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 // This test does not work for some reason. Dunno why
 // @SpringBootTest annotation works with the same test
 // But @SpringJUnitConfig doesn't.
 
+// Update to above:
+// This test works now. Had to change @PropertySource to @TestPropertySource
+
 // We are telling this annotation exactly what configuration to use
 // and that configuration will scan the entire package structure
-@SpringJUnitConfig(value = App.class)
-@ActiveProfiles("dev") // Note without specifying the profile, we are getting error during autowiring the bean
-@PropertySource({"classpath:application.properties"})
+@SpringJUnitConfig(value = TestConfig.class)
+//@ActiveProfiles("dev") // Note without specifying the profile, we are getting error during autowiring the bean (actually, not anymore)
+@TestPropertySource(locations = {"classpath:application-test.properties", "classpath:test.properties"})
 public class ProjectServiceIntegrationTest {
 
     @Autowired
     @Qualifier("projectServiceImpl")
     private IProjectService projectService;
+
+    // You can how we are reading value from 2 different property files
+    // additional.info is defined in test.properties
+    @Value("${additional.info}")
+    private String additionalValue;
+
+    // project.prefix is defined in application-test.properties
+    @Value("${project.prefix}")
+    private String projectPrefix;
+
+    @Test
+    public void whenTestPropertySource_thenValuesRetrieved() {
+        // Note the import that is being used for the assertEquals
+        // import static org.junit.jupiter.api.Assertions.assertEquals;
+        assertEquals("Additional Info from test.properties", additionalValue);
+    }
+
+    @Test
+    public void whenTestPropertySource_thenValuesRetrieved2() {
+        assertEquals("PRO-TEST", projectPrefix);
+    }
 
     @Test
     public void whenSavingProject_thenOK(){
@@ -34,5 +60,4 @@ public class ProjectServiceIntegrationTest {
         Project savedProj = projectService.save(project);
         assertNotNull(savedProj);
     }
-
 }
--- src/test/java/com/example/rgbk/ProjectServiceIntegrationTest2.java
+++ src/test/java/com/example/rgbk/ProjectServiceIntegrationTest2.java
@@ -5,21 +5,41 @@ import com.example.rgbk.service.IProjectService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:application-test.properties", "classpath:test.properties"})
 public class ProjectServiceIntegrationTest2 {
 
     @Autowired
     @Qualifier("projectServiceImpl")
     private IProjectService projectService;
 
+    @Value("${project.prefix}")
+    private String projectPrefix;
+
+    @Value("${additional.info}")
+    private String additionalValue;
+
     @Test
-    public void whenSavingProject_thenOK(){
+    public void whenTestPropertySource_thenValuesRetrieved() {
+        assertEquals("Additional Info from test.properties", additionalValue);
+    }
+
+    @Test
+    public void whenTestPropertySource_thenValuesRetrieved2() {
+        assertEquals("PRO-TEST", projectPrefix);
+    }
+
+    @Test
+    public void whenSavingProject_thenOK() {
         Project project = new Project("A New Project", LocalDate.now());
         Project savedProj = projectService.save(project);
         assertNotNull(savedProj);
